f938ab0b8fbcc50a07640179771f37ffa6db60d4
Dave Carroll <37391437+thedavecarroll@users.noreply.github.com>
2018-06-06 23:09:21 -0500

    switch to new pipeline method

--- PoShEvents/Public/Get-SystemRestartEvent.ps1
+++ PoShEvents/Public/Get-SystemRestartEvent.ps1
@@ -2,7 +2,7 @@
     [CmdletBinding()]
     param(
         [Parameter(ValueFromPipelineByPropertyName=$true,ValueFromPipeline=$true)]
-        [Alias('IPAddress','__Server','CN')]      
+        [Alias('IPAddress','__Server','CN')]
         [string[]]$ComputerName='localhost',
         [ValidateNotNull()]
         [System.Management.Automation.PSCredential]
@@ -37,58 +37,7 @@
 
     process {
 
-        $Events = Get-MyEvent -ComputerName $ComputerName -FilterHashtable $FilterHashTable @ParameterSplat
-    
-        $EventCount = 0
-        foreach ($Event in $Events) {
-            if ($EventCount -gt 0) {
-                Write-Progress -Id 1 -Activity "Formatting events..." -PercentComplete (($EventCount / $Events.count) * 100)
-            }
-            $EventCount++
-            
-            $EventLogRecord = ConvertFrom-EventLogRecord -EventLogRecord $Event
-
-            if ($EventLogRecord.Id -eq 1074) {
-                $UserName = $EventLogRecord.param7
-                $Reason = $EventLogRecord.param3
-                if ($EventLogRecord.param1 -match "CcmExec") {
-                    $Details = "Configuration Manager"
-                } elseif ($EventLogRecord.param1 -match "msiexe") {
-                    $Details = "Windows Installer"
-                } elseif ($EventLogRecord.param1 -match "vmtoolsd") {
-                    $Details = "VMTools"
-                } elseif ($EventLogRecord.param1 -match "FveNotify") {
-                    $Details = "BitLocker"
-                } else {
-                    $Details = $EventLogRecord.param1.Split(" ")[0].Split("\")[-1]
-                }
-
-            } else {
-                $UserName = $null
-                $Reason = $null
-                $Details = $null
-            }
-
-            switch ($EventLogRecord.id) {
-                6005 { $Status = "Startup" }
-                6006 { $Status = "Shutdown" }
-                6008 { $Status = "Unexpected Shutdown" }
-                6009 { $Status = "System Info" }
-                1074 { $Status = "Shutdown Initiated" }
-            }
-
-            [PSCustomObject]@{
-                ComputerName = $EventLogRecord.ComputerName
-                TimeCreated = $EventLogRecord.TimeCreated
-                Id = $EventLogRecord.Id
-                ProviderName = $EventLogRecord.ProviderName
-                Level = $EventLogRecord.Level
-                Status = $Status 
-                UserName = $UserName
-                Reason = $Reason
-                Details = $Details
-            } 
-        }
+        Get-MyEvent -ComputerName $ComputerName -FilterHashtable $FilterHashTable @ParameterSplat | ConvertFrom-EventLogRecord -EventRecordType 'SystemRestartEvent'
     }
 
     end {
