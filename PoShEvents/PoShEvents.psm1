48a1466dc27c31d19bb9aa2fe2db4a6d76b0662c
Dave Carroll <37391437+thedavecarroll@users.noreply.github.com>
2018-06-06 23:08:37 -0500

    import format and type data

--- PoShEvents/PoShEvents.psm1
+++ PoShEvents/PoShEvents.psm1
@@ -6,13 +6,29 @@ try {
     foreach ($Scope in 'Public','Private') {
         Get-ChildItem "$PSScriptRoot\$Scope" -Filter *.ps1 | ForEach-Object {
             . $_.FullName
-            if ($Scope -eq 'Public') { 
+            if ($Scope -eq 'Public') {
                 Export-ModuleMember -Function $_.BaseName -ErrorAction Stop
-            }            
+            }
         }
     }
-} 
+}
 catch {
     Write-Error ("{0}: {1}" -f $_.BaseName,$_.Exception.Message)
+    exit 1
+}
+
+# import format and type data
+Try {
+    Update-FormatData "$PSScriptRoot\TypeData\PoShEvents.Format.ps1xml" -ErrorAction Stop
+}
+catch {
+    Write-Error ("{0}: {1}" -f 'Update-FormatData',$_.Exception.Message)
+    exit 1
+}
+try {
+    Update-TypeData "$PSScriptRoot\TypeData\PoShEvents.Types.ps1xml" -ErrorAction Stop
+}
+catch {
+    Write-Error ("{0}: {1}" -f 'Update-TypeData',$_.Exception.Message)
     exit 1
 }
