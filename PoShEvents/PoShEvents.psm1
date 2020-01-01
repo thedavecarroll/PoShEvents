48ff69cd161accab9c40ff9a7c62d0a6457f88f0
Dave Carroll <powershell@anovelidea.org>
2019-12-31 19:56:33 -0600

    minor update

--- PoShEvents/PoShEvents.psm1
+++ PoShEvents/PoShEvents.psm1
@@ -1,10 +1,25 @@
 [CmdLetBinding()]
 param()
 
-# dot source public and private function definition files, export public functions
+#region info
+<#
+The following members are exported via the module's data file (.psd1)
+    Functions
+    TypeData
+    FormatData
+#>
+#endregion info
+
+#region discover module name
+$ScriptPath = Split-Path $MyInvocation.MyCommand.Path
+$ModuleName = $ExecutionContext.SessionState.Module
+Write-Verbose -Message "Loading module $ModuleName"
+#endregion discover module name
+
+#region dot source public and private function definition files, export publich functions
 try {
     foreach ($Scope in 'Public','Private') {
-        Get-ChildItem "$PSScriptRoot\$Scope" -Filter *.ps1 | ForEach-Object {
+        Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath $Scope) -Filter *.ps1 | ForEach-Object {
             . $_.FullName
             if ($Scope -eq 'Public') {
                 Export-ModuleMember -Function $_.BaseName -ErrorAction Stop
@@ -16,3 +31,4 @@ catch {
     Write-Error ("{0}: {1}" -f $_.BaseName,$_.Exception.Message)
     exit 1
 }
+#endregion dot source public and private function definition files, export publich functions
