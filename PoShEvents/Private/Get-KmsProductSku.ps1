84d2b05d31e7601b485163148c7950599936d42f
Dave Carroll <powershell@anovelidea.org>
2020-01-03 21:23:19 -0600

    closes #26

--- PoShEvents/Private/Get-KmsProductSku.ps1
+++ PoShEvents/Private/Get-KmsProductSku.ps1
@@ -1,9 +1,11 @@
 function Get-KmsProductSku {
+    [CmdLetBinding()]
     param([string]$ProductSku)
     # see https://github.com/asaboss/py-kms-server/ for updates
     # check https://github.com/CNMan/balala/blob/master/pkconfig.csv for more updates
 
-    $KmsProductSku = Import-Csv -Path .\KmsProductSku.csv
+    $KmsProductSkuPath = Join-Path -Path $PSScriptRoot -ChildPath 'KmsProductSku.csv'
+    $KmsProductSku = Import-Csv -Path $KmsProductSkuPath
     if ($KmsProductSku.Where({$_.ActConfigID -match $ProductSku})) {
         $KmsProductSku.Where({$_.ActConfigID -match $ProductSku}).ProductDescription
     } else {
